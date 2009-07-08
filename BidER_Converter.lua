2d713d5105fd66e8092c6011a2cc79f09b9457e0
friedmag <friedmagb@gmail.com>
2009-07-08 19:35:27 -0400

    Initial very simple post-creator.  And the current data as of last night.

---
+++ BidER_Converter.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env lua
+
+require('BidER')
+
+local dkpset = "official"
+if arg[1] ~= nil then
+  dkpset = arg[1]
+end
+
+local dkp = BidER_DKP[dkpset]
+local names = {}
+for i,v in pairs(dkp) do
+  table.insert(names, i)
+end
+table.sort(names)
+for i,v in ipairs(names) do
+  print(v .. ": " .. dkp[v].total)
+end
