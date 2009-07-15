ed781e7f5580d848bf842802931fd28f53328b04
friedmag <friedmagb@gmail.com>
2009-07-15 11:45:58 -0400

    Updated for 7/14/09

--- BidER.lua
+++ BidER.lua
@@ -14,7 +14,7 @@ BidER_DKP = {
 			["total"] = "5",
 		},
 		["Midknyte"] = {
-			["total"] = 1,
+			["total"] = 2,
 		},
 		["Sheemie"] = {
 			["total"] = "4",
@@ -26,7 +26,7 @@ BidER_DKP = {
 			["total"] = 101,
 		},
 		["Alok"] = {
-			["total"] = 2,
+			["total"] = 8,
 		},
 		["Ginaden"] = {
 			["total"] = "12",
@@ -41,13 +41,13 @@ BidER_DKP = {
 			["total"] = 2,
 		},
 		["Bukthan"] = {
-			["total"] = 11,
+			["total"] = 1,
 		},
 		["Rumbler"] = {
-			["total"] = 17,
+			["total"] = 23,
 		},
 		["Bradac"] = {
-			["total"] = "0",
+			["total"] = 6,
 		},
 		["Sindd"] = {
 			["total"] = "20",
@@ -71,7 +71,7 @@ BidER_DKP = {
 			["total"] = "2",
 		},
 		["Gehzry"] = {
-			["total"] = 1,
+			["total"] = 7,
 		},
 		["Dook"] = {
 			["total"] = 11,
@@ -82,6 +82,9 @@ BidER_DKP = {
 		["Severinna"] = {
 			["total"] = "6",
 		},
+		["Gylak"] = {
+			["total"] = 13,
+		},
 		["Vintarius"] = {
 			["total"] = "11",
 		},
@@ -89,13 +92,13 @@ BidER_DKP = {
 			["total"] = "13",
 		},
 		["Quilderaumo"] = {
-			["total"] = 1,
+			["total"] = 7,
 		},
 		["Namefear"] = {
 			["total"] = "10",
 		},
 		["Selidar"] = {
-			["total"] = 46,
+			["total"] = 52,
 		},
 		["Pyriall"] = {
 			["total"] = "2",
@@ -107,7 +110,7 @@ BidER_DKP = {
 			["total"] = "17",
 		},
 		["Tairy"] = {
-			["total"] = 5,
+			["total"] = 11,
 		},
 		["Ztyx"] = {
 			["total"] = "2",
@@ -131,13 +134,13 @@ BidER_DKP = {
 			["total"] = "4",
 		},
 		["Shyzyhra"] = {
-			["total"] = 39,
+			["total"] = 2,
 		},
 		["Melbindi"] = {
-			["total"] = 9,
+			["total"] = 0,
 		},
 		["Tarnaa"] = {
-			["total"] = 4,
+			["total"] = 10,
 		},
 		["Keelintaro"] = {
 			["total"] = "13",
@@ -149,13 +152,13 @@ BidER_DKP = {
 			["total"] = "0",
 		},
 		["Withal"] = {
-			["total"] = 16,
+			["total"] = 3,
 		},
 		["Vengent"] = {
 			["total"] = "16",
 		},
 		["Visslik"] = {
-			["total"] = 13,
+			["total"] = 19,
 		},
 		["Dacar"] = {
 			["total"] = "7",
@@ -185,7 +188,7 @@ BidER_DKP = {
 			["total"] = "15",
 		},
 		["Vrisha"] = {
-			["total"] = 9,
+			["total"] = 5,
 		},
 		["Jillien"] = {
 			["total"] = "0",
@@ -194,7 +197,7 @@ BidER_DKP = {
 			["total"] = "57",
 		},
 		["Coolleen"] = {
-			["total"] = 11,
+			["total"] = 5,
 		},
 		["Cleophus"] = {
 			["total"] = 9,
@@ -203,7 +206,7 @@ BidER_DKP = {
 			["total"] = "0",
 		},
 		["Keeneyes"] = {
-			["total"] = 12,
+			["total"] = 18,
 		},
 		["Aanelorn"] = {
 			["total"] = "2",
@@ -239,7 +242,7 @@ BidER_DKP = {
 			["total"] = "8",
 		},
 		["Ariaxx"] = {
-			["total"] = 9,
+			["total"] = 1,
 		},
 		["Borialis"] = {
 			["total"] = "5",
@@ -248,16 +251,16 @@ BidER_DKP = {
 			["total"] = "4",
 		},
 		["Gylan"] = {
-			["total"] = 3,
+			["total"] = 13,
 		},
 		["Lenadin"] = {
 			["total"] = "14",
 		},
 		["Mzammer"] = {
-			["total"] = 5,
+			["total"] = 0,
 		},
 		["Riçkkëz"] = {
-			["total"] = 8,
+			["total"] = 4,
 		},
 		["Kalestra"] = {
 			["total"] = 31,
@@ -302,7 +305,7 @@ BidER_DKP = {
 			["total"] = "2",
 		},
 		["Gelnon"] = {
-			["total"] = 1,
+			["total"] = 4,
 		},
 		["Lien"] = {
 			["total"] = "2",
@@ -341,7 +344,7 @@ BidER_DKP = {
 			["total"] = "72",
 		},
 		["Pokit"] = {
-			["total"] = 1,
+			["total"] = 4,
 		},
 		["Jurion"] = {
 			["total"] = "1",
@@ -350,7 +353,7 @@ BidER_DKP = {
 			["total"] = "10",
 		},
 		["Valore"] = {
-			["total"] = 0,
+			["total"] = 6,
 		},
 		["Palytank"] = {
 			["total"] = "0",
@@ -365,10 +368,10 @@ BidER_DKP = {
 			["total"] = "15",
 		},
 		["Infyrno"] = {
-			["total"] = 1,
+			["total"] = 7,
 		},
 		["Scornhorn"] = {
-			["total"] = 13,
+			["total"] = 0,
 		},
 		["Blyte"] = {
 			["total"] = "86",
@@ -377,7 +380,7 @@ BidER_DKP = {
 			["total"] = "7",
 		},
 		["Lunastra"] = {
-			["total"] = 12,
+			["total"] = 18,
 		},
 		["Embrr"] = {
 			["total"] = 6,
@@ -395,7 +398,7 @@ BidER_DKP = {
 			["total"] = "0",
 		},
 		["Valsarius"] = {
-			["total"] = 9,
+			["total"] = 0,
 		},
 		["Astoy"] = {
 			["total"] = "0",
@@ -407,7 +410,7 @@ BidER_DKP = {
 			["total"] = "3",
 		},
 		["Rarebird"] = {
-			["total"] = 5,
+			["total"] = 6,
 		},
 		["Enaru"] = {
 			["total"] = "4",
@@ -416,7 +419,7 @@ BidER_DKP = {
 			["total"] = 1,
 		},
 		["Silvanara"] = {
-			["total"] = 14,
+			["total"] = 0,
 		},
 	},
 }
@@ -437,6 +440,14 @@ BidER_DKPResets = {
 		"Midknyte", -- [13]
 		"Gelnon", -- [14]
 		"Quilderaumo", -- [15]
+		"Vrisha", -- [16]
+		"Valsarius", -- [17]
+		"Coolleen", -- [18]
+		"Riçkkëz", -- [19]
+		"Withal", -- [20]
+		"Shyzyhra", -- [21]
+		"Silvanara", -- [22]
+		"Scornhorn", -- [23]
 	},
 }
 BidER_Settings = {
@@ -453,9 +464,305 @@ BidER_MinimumBids = {
 }
 BidER_Aliases = {
 	["Melbindi"] = "Valsarius",
-	["Pokit"] = "Gelnon",
 	["Gehzry"] = "Quilderaumo",
+	["Pokit"] = "Gelnon",
 	["Gylak"] = "Gylan",
 }
-BidER_Exports = {
+BidER_Raids = {
+	[1247619614] = {
+		["end_time"] = 1247630555,
+		["start_time"] = 1247619614,
+		["events"] = {
+			["Flame Leviathan"] = {
+				["attendance"] = {
+					"Bukthan", -- [1]
+					"Pokit", -- [2]
+					"Vrisha", -- [3]
+					"Infyrno", -- [4]
+					"Alok", -- [5]
+					"Lunastra", -- [6]
+					"Tarnaa", -- [7]
+					"Valsarius", -- [8]
+					"Riçkkëz", -- [9]
+					"Selidar", -- [10]
+					"Bradac", -- [11]
+					"Valore", -- [12]
+					"Shyzyhra", -- [13]
+					"Silvanara", -- [14]
+					"Tairy", -- [15]
+					"Coolleen", -- [16]
+					"Midknyte", -- [17]
+					"Rumbler", -- [18]
+					"Keeneyes", -- [19]
+					"Visslik", -- [20]
+					"Rarebird", -- [21]
+					"Gylan", -- [22]
+					"Mzammer", -- [23]
+					"Quilderaumo", -- [24]
+				},
+				["killed"] = true,
+				["loots"] = {
+					{
+						["amount"] = 10,
+						["item"] = "|cffa335ee|Hitem:45112:0:0:0:0:0:0:1337178432:80|h[The Leviathan's Coil]|h|r",
+						["who"] = "Vrisha",
+					}, -- [1]
+					{
+						["amount"] = 10,
+						["item"] = "|cffa335ee|Hitem:45106:0:0:0:0:0:0:1068742976:80|h[Strength of the Automaton]|h|r",
+						["who"] = "Valsarius",
+					}, -- [2]
+					{
+						["amount"] = 12,
+						["item"] = "|cffa335ee|Hitem:45117:0:0:0:0:0:0:1605613888:80|h[Constructor's Handwraps]|h|r",
+						["who"] = "Coolleen",
+					}, -- [3]
+				},
+				["attempts"] = {
+					1247620910, -- [1]
+				},
+			},
+			["Kologarn"] = {
+				["attendance"] = {
+					"Bukthan", -- [1]
+					"Pokit", -- [2]
+					"Vrisha", -- [3]
+					"Infyrno", -- [4]
+					"Alok", -- [5]
+					"Lunastra", -- [6]
+					"Tarnaa", -- [7]
+					"Valsarius", -- [8]
+					"Riçkkëz", -- [9]
+					"Selidar", -- [10]
+					"Bradac", -- [11]
+					"Valore", -- [12]
+					"Shyzyhra", -- [13]
+					"Silvanara", -- [14]
+					"Tairy", -- [15]
+					"Coolleen", -- [16]
+					"Midknyte", -- [17]
+					"Rumbler", -- [18]
+					"Keeneyes", -- [19]
+					"Visslik", -- [20]
+					"Withal", -- [21]
+					"Gylan", -- [22]
+					"Gylak", -- [23]
+					"Quilderaumo", -- [24]
+				},
+				["killed"] = true,
+				["loots"] = {
+					{
+						["amount"] = 5,
+						["item"] = "|cffa335ee|Hitem:45262:0:0:0:0:0:0:677333600:80|h[Necklace of Unerring Mettle]|h|r",
+						["who"] = "Midknyte",
+					}, -- [1]
+					{
+						["amount"] = 10,
+						["item"] = "|cffa335ee|Hitem:45267:0:0:0:0:0:0:945769056:80|h[Saronite Plated Legguards]|h|r",
+						["who"] = "Bukthan",
+					}, -- [2]
+					{
+						["amount"] = 43,
+						["item"] = "|cffa335ee|Hitem:45272:0:0:0:0:0:0:1214204512:80|h[Robes of the Umbral Brute]|h|r",
+						["who"] = "Shyzyhra",
+					}, -- [3]
+				},
+				["attempts"] = {
+					1247625302, -- [1]
+				},
+			},
+			["XT-002 Deconstructor"] = {
+				["attendance"] = {
+					"Bukthan", -- [1]
+					"Pokit", -- [2]
+					"Vrisha", -- [3]
+					"Infyrno", -- [4]
+					"Alok", -- [5]
+					"Lunastra", -- [6]
+					"Tarnaa", -- [7]
+					"Valsarius", -- [8]
+					"Riçkkëz", -- [9]
+					"Selidar", -- [10]
+					"Bradac", -- [11]
+					"Valore", -- [12]
+					"Shyzyhra", -- [13]
+					"Silvanara", -- [14]
+					"Tairy", -- [15]
+					"Coolleen", -- [16]
+					"Midknyte", -- [17]
+					"Rumbler", -- [18]
+					"Keeneyes", -- [19]
+					"Visslik", -- [20]
+					"Withal", -- [21]
+					"Gylan", -- [22]
+					"Mzammer", -- [23]
+					"Gylak", -- [24]
+					"Quilderaumo", -- [25]
+				},
+				["killed"] = true,
+				["loots"] = {
+					{
+						["amount"] = 3,
+						["item"] = "|cffa335ee|Hitem:45258:0:0:0:0:0:0:1770807168:80|h[Sandals of Rash Temperament]|h|r",
+						["who"] = "Pokit",
+					}, -- [1]
+					{
+						["amount"] = 10,
+						["item"] = "|cffa335ee|Hitem:45248:0:0:0:0:0:0:1233936256:80|h[Clockwork Legplates]|h|r",
+						["who"] = "Riçkkëz",
+					}, -- [2]
+				},
+				["attempts"] = {
+					1247622495, -- [1]
+				},
+			},
+			["Ignis the Furnace Master"] = {
+				["attendance"] = {
+					"Bukthan", -- [1]
+					"Pokit", -- [2]
+					"Vrisha", -- [3]
+					"Infyrno", -- [4]
+					"Alok", -- [5]
+					"Lunastra", -- [6]
+					"Tarnaa", -- [7]
+					"Valsarius", -- [8]
+					"Riçkkëz", -- [9]
+					"Selidar", -- [10]
+					"Bradac", -- [11]
+					"Valore", -- [12]
+					"Shyzyhra", -- [13]
+					"Silvanara", -- [14]
+					"Ariaxx", -- [15]
+					"Coolleen", -- [16]
+					"Midknyte", -- [17]
+					"Rumbler", -- [18]
+					"Gylak", -- [19]
+					"Visslik", -- [20]
+					"Tairy", -- [21]
+					"Withal", -- [22]
+					"Scornhorn", -- [23]
+					"Keeneyes", -- [24]
+					"Quilderaumo", -- [25]
+				},
+				["killed"] = true,
+				["loots"] = {
+					{
+						["amount"] = 20,
+						["item"] = "|cffa335ee|Hitem:45170:0:0:0:0:0:0:889066392:80|h[Scepter of Creation]|h|r",
+						["who"] = "Silvanara",
+					}, -- [1]
+					{
+						["amount"] = 5,
+						["item"] = "|cffa335ee|Hitem:45161:0:0:0:0:0:0:352195480:80|h[Girdle of Embers]|h|r",
+						["who"] = "Valsarius",
+					}, -- [2]
+					{
+						["amount"] = 14,
+						["item"] = "|cffa335ee|Hitem:45166:0:0:0:0:0:0:620630936:80|h[Charred Saronite Greaves]|h|r",
+						["who"] = "Scornhorn",
+					}, -- [3]
+				},
+				["attempts"] = {
+					1247629615, -- [1]
+				},
+			},
+			["Auriaya"] = {
+				["attendance"] = {
+					"Bukthan", -- [1]
+					"Pokit", -- [2]
+					"Vrisha", -- [3]
+					"Infyrno", -- [4]
+					"Alok", -- [5]
+					"Lunastra", -- [6]
+					"Tarnaa", -- [7]
+					"Valsarius", -- [8]
+					"Riçkkëz", -- [9]
+					"Selidar", -- [10]
+					"Bradac", -- [11]
+					"Valore", -- [12]
+					"Shyzyhra", -- [13]
+					"Silvanara", -- [14]
+					"Coolleen", -- [15]
+					"Midknyte", -- [16]
+					"Rumbler", -- [17]
+					"Gylak", -- [18]
+					"Visslik", -- [19]
+					"Tairy", -- [20]
+					"Gylan", -- [21]
+					"Withal", -- [22]
+					"Keeneyes", -- [23]
+					"Quilderaumo", -- [24]
+				},
+				["killed"] = true,
+				["loots"] = {
+					{
+						["amount"] = 6,
+						["item"] = "|cffa335ee|Hitem:45319:0:0:0:0:0:0:1513475328:80|h[Cloak of the Makers]|h|r",
+						["who"] = "Bukthan",
+					}, -- [1]
+					{
+						["amount"] = 10,
+						["item"] = "|cffa335ee|Hitem:45437:0:0:0:0:0:0:2050346240:80|h[Runescribed Blade]|h|r",
+						["who"] = "Ariaxx",
+					}, -- [2]
+				},
+				["attempts"] = {
+					1247626440, -- [1]
+					1247626976, -- [2]
+					1247627928, -- [3]
+				},
+			},
+			["Razorscale"] = {
+				["attendance"] = {
+					"Bukthan", -- [1]
+					"Pokit", -- [2]
+					"Vrisha", -- [3]
+					"Infyrno", -- [4]
+					"Alok", -- [5]
+					"Lunastra", -- [6]
+					"Tarnaa", -- [7]
+					"Valsarius", -- [8]
+					"Riçkkëz", -- [9]
+					"Selidar", -- [10]
+					"Bradac", -- [11]
+					"Valore", -- [12]
+					"Shyzyhra", -- [13]
+					"Silvanara", -- [14]
+					"Tairy", -- [15]
+					"Coolleen", -- [16]
+					"Midknyte", -- [17]
+					"Rumbler", -- [18]
+					"Keeneyes", -- [19]
+					"Visslik", -- [20]
+					"Withal", -- [21]
+					"Gylan", -- [22]
+					"Mzammer", -- [23]
+					"Gylak", -- [24]
+					"Quilderaumo", -- [25]
+				},
+				["killed"] = true,
+				["loots"] = {
+					{
+						["amount"] = 8,
+						["item"] = "|cffa335ee|Hitem:45150:0:0:0:0:0:0:-1345924992:80|h[Collar of the Wyrmhunter]|h|r",
+						["who"] = "Mzammer",
+					}, -- [1]
+					{
+						["amount"] = 18,
+						["item"] = "|cffa335ee|Hitem:45142:0:0:0:0:0:0:-1614360448:80|h[Remorse]|h|r",
+						["who"] = "Withal",
+					}, -- [2]
+					{
+						["amount"] = 3,
+						["item"] = "|cffa335ee|Hitem:45140:0:0:0:0:0:0:-1882795904:80|h[Razorscale Shoulderguards]|h|r",
+						["who"] = "Valore",
+					}, -- [3]
+				},
+				["attempts"] = {
+					1247623591, -- [1]
+				},
+			},
+		},
+		["zone"] = "Ulduar",
+	},
 }
